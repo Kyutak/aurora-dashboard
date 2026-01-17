23fd3f37a061b6b79b3717633f5af69f48a84fa4
Erick200723 <erick2007gabriel23@gmail.com>
2026-01-16 23:10:41 -0300

    trocando url para backend no render

--- service/api.ts
+++ service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://aurora-api-095s.onrender.com',
    withCredentials: true,
 });
 
