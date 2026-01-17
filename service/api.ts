d3f32a9760770a8e0bace617b1f34809a730c7d2
Erick200723 <erick2007gabriel23@gmail.com>
2026-01-16 23:12:16 -0300

    reajuste de logica de cooke

--- service/api.ts
+++ service/api.ts
@@ -4,11 +4,3 @@ export const api = axios.create({
   baseURL: 'https://aurora-api-095s.onrender.com',
    withCredentials: true,
 });
-
-api.interceptors.request.use(config => {
-  const token = sessionStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
