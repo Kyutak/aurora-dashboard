233a42c1965bfa65ca20f2f90daa565cdb258aaa
Erick200723 <erick2007gabriel23@gmail.com>
2026-01-16 21:56:05 -0300

    ...

--- middleware.ts
+++ middleware.ts
@@ -14,7 +14,7 @@ export function middleware(req: NextRequest) {
     return NextResponse.next()
   }
 
-  if( pathname.startsWith("/auth") && pathname === "/" ){
+  if( pathname.startsWith("/auth") ){
     return NextResponse.next()
   } 
 
